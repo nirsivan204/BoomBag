43a787cb61842b0fd63958f845ff3bab9f110d25
Uri Leon Freund <73687539+BalthazaarXIV@users.noreply.github.com>
2021-02-19 12:25:37 +0200

    Color Changer Script
    Created color changing script which currently works based on a time and repeat parameter, and can change the color using an array.

---
+++ Assets/Scripts/Managers/ColorChangeManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ColorChangeManager : MonoBehaviour
+{
+    private Color[] meshColors;
+    public GameObject[] players;
+    private MeshRenderer[] meshRenderers;
+    
+    public void Start()
+    {
+        // Set Hex colors to Color name in array
+        ColorUtility.TryParseHtmlString("#CDD764", out Color yellow); 
+        ColorUtility.TryParseHtmlString("#E63430", out Color red);
+        ColorUtility.TryParseHtmlString("#61C2C8", out Color cyan);
+        ColorUtility.TryParseHtmlString("#EDA853", out Color orange);
+        ColorUtility.TryParseHtmlString("#00A650", out Color green);
+        ColorUtility.TryParseHtmlString("#81398F", out Color purple);
+        meshColors = new Color[] {yellow, red, cyan, orange, green, purple};
+        meshRenderers = new MeshRenderer[players.Length];
+        
+        for (int i = 0; i < players.Length; i++)
+        {
+            meshRenderers[i] = players[i].GetComponent<MeshRenderer>();
+        }
+
+        InvokeRepeating("ColorChanger", 3, 3);
+    }
+
+    public void ColorChanger()
+    {
+        foreach (MeshRenderer playerMesh in meshRenderers)
+        {
+            playerMesh.material.color = meshColors[Random.Range(0, meshColors.Length)];
+        }
+    }
+}
