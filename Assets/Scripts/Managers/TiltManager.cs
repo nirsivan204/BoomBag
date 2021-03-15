4cb4daf1213c0aab35d63d034a4ec01e83354086
avieli <avieli@gmail.com>
2021-03-15 08:31:33 +0200

    Change tilt to work with all objects with ground tag

--- Assets/Scripts/Managers/TiltManager.cs
+++ Assets/Scripts/Managers/TiltManager.cs
@@ -4,15 +4,15 @@ using UnityEngine.InputSystem;
 
 public class TiltManager : MonoBehaviour
 {
-    private GameObject ground;
+    private GameObject[] grounds;
     private float timer, tiltAngle, tiltTime;
     private bool isTilting;
     private Vector3 tiltVector;
-    private const float MAX_TILT = 0.1f;
+    // private const float MAX_TILT = 0.1f;
 
     void Start()
     {
-        ground = GameObject.Find("BaseGround"); // Probably should use a tag.
+        grounds = GameObject.FindGameObjectsWithTag("Arena");
         timer = 0;
     }
 
@@ -21,19 +21,18 @@ public class TiltManager : MonoBehaviour
     private void OnTilt(InputValue lookValue)
 	{
         Vector2 v = lookValue.Get<Vector2>();
-        startTilt(new Vector3(v.x, 0, v.y), 0.1f * v.magnitude, 0.1f);
+        startTilt(new Vector3(v.y, 0, -v.x), 0.1f * v.magnitude, 0.1f);  // 2d y is placed in 3d z.
     }
 #endif
 
     // Tilt angle around axis (unless it's more then MAX_TILT).
     void tilt(Vector3 axis, float angle)
 	{
-        ground.transform.Rotate(axis, angle);
-        if (Mathf.Abs(ground.transform.rotation.x) > MAX_TILT || Mathf.Abs(ground.transform.rotation.z) > MAX_TILT)
+        foreach (GameObject ground in grounds)
         {
-            
-            ground.transform.Rotate(axis, -angle);
+            ground.transform.Rotate(axis, angle, Space.World);
         }
+        // TODO: Add max tilt.
     }
 
     // Set the parameters to start tilting. The actual tilt happens in FixedUpdate.
@@ -45,6 +44,7 @@ public class TiltManager : MonoBehaviour
         isTilting = true;
     }
 
+    // Currently tilting is done by changing the transform. It is more correct to do it by applying force on a rigid body. But the ground being a rigid body has its problems...
     void FixedUpdate()
     {
         // Tilt slowly over some frames:
