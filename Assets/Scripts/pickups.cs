341d2704e25dff74d6ef8476d03ac3a2c4e12923
nir sivan <nirsivan204@gmail.com>
2021-04-08 03:41:25 +0300

    pickables

---
+++ Assets/Scripts/pickups.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+    public enum pickupsTypes { ENLARGE, SHRINK, INVERTER, FULLPOWER, BOMB }
+}
