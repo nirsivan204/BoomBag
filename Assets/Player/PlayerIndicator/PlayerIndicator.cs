42a175cac958bd5d3c4085a658fe28dd43b47f32
mambojambo3000 <66967082+mambojambo3000@users.noreply.github.com>
2021-11-13 18:21:02 +0200

    Add Player Indicator Graphics not coded

--- Assets/Player/PlayerIndicator/PlayerIndicator.cs
+++ Assets/Player/PlayerIndicator/PlayerIndicator.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerIndicator : MonoBehaviour {
@@ -24,13 +25,23 @@ public class PlayerIndicator : MonoBehaviour {
     void Update ()
     {
         // Spin object around Y-Axis
-        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+        // transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
  
         // Float up/down with a Sin()
-        tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
-        transform.localPosition = tempPos + new Vector3(0, 2.2f , 0);
+           tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
+           transform.localPosition = tempPos + new Vector3(0, 2.2f , 0);
+       
         
         // Fill the indicator according to player's energy:
-        mesh.material.SetTextureOffset("_MainTex" , new Vector2(0, parentPlayer.energy / (AbstractPlayer.MAX_ENERGY * 2)));
+       // mesh.material.SetTextureOffset("_MainTex" , new Vector2(0, parentPlayer.energy / (AbstractPlayer.MAX_ENERGY * 2)));
+       
+       
+    }
+
+
+    private void LateUpdate()
+    {
+        // Look at Camera
+        transform.LookAt(Camera.main.transform.position, Vector3.up);
     }
 }
